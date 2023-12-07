a9f210bd1e5510c059afb3b6307e7288c6663784
chandrika-bandaru <149655493+chandrika-bandaru@users.noreply.github.com>
2023-12-07 12:09:21 +0530

    #include<stdio.h> int main() {     int x,y,z;     scanf("%d%d%d",&x,&y,&z);     int res=((x*5)+(y*10))/z;     printf("%d",res); }

--- Romeo_and_Juliet.c
+++ Romeo_and_Juliet.c
@@ -3,6 +3,6 @@ int main()
 {
     int x,y,z;
     scanf("%d%d%d",&x,&y,&z);
-    int numofchoclates=((x*5)+(y*10))/z;
-    printf("%d",numofchoclates);
+    int res=((x*5)+(y*10))/z;
+    printf("%d",res);
 }
